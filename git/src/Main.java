04b92b8b07d9aaa04360f3bf117c3a7084421ea5
mawz115 <mawzfire@163.com>
2020-03-28 14:47:33 +0800

    第五次

--- git/src/Main.java
+++ git/src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World--5!");
+        System.out.println("Hello World--2!");
         //第一次修改
         //第二次修改
-        //第五次修改
     }
 }
